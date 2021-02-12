be681b28cee182be73902096c69ee053587ef58c
Aidan Hill <aidanchill@gmail.com>
2021-02-12 11:26:56 -0500

    add task functionality

--- src/App.js
+++ src/App.js
@@ -27,7 +27,9 @@ function App({ title, text }) {
 
 // Add task
 const addTask = (task) => {
-  console.log(task)
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
 }
 
 // Delete task
